adfb610b17bc1ad316f216dcb7c2acc758205fd8
abdulaziz <qwertyabu2007@gmail.com>
2026-05-30 00:13:09 +0900

    To keep the last chosen color

--- WhiteboardProject/src/gui/CanvasPanel.java
+++ WhiteboardProject/src/gui/CanvasPanel.java
@@ -9,6 +9,8 @@ import java.awt.image.BufferedImage;
 import javax.swing.JOptionPane;
 
 public class CanvasPanel extends JPanel {
+    private BufferedImage canvas;
+    private Graphics2D obj2d;
     int lastX, lastY;
     boolean drawing = false;
     public CanvasPanel(){
